2f315394818543def69d167a126a9bce7593a1c8
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-03-13 17:06:17 -0400

    Moved stuff to config.lua

--- client/main.lua
+++ client/main.lua
@@ -1,32 +1,3 @@
--- BELOVE IS YOUR SETTINGS, CHANGE THEM TO WHATEVER YOU'D LIKE & MORE SETTINGS WILL COME IN THE FUTURE! --
-
-local useBilling = true -- OPTIONS: (true/false)
-local useCameraSound = true -- OPTIONS: (true/false)
-local useFlashingScreen = true -- OPTIONS: (true/false)
-local useBlips = true -- OPTIONS: (true/false)
-local alertPolice = true -- OPTIONS: (true/false)
-local alertSpeed = 150 -- OPTIONS: (1-5000 KMH)
-
--- ABOVE IS YOUR SETTINGS, CHANGE THEM TO WHATEVER YOU'D LIKE & MORE SETTINGS WILL COME IN THE FUTURE!  --
-
-
-
-
-
-
-
-local Keys = {
-  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
-  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
-  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
-  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-  ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
-
 ESX = nil
 
 local hasBeenCaught = false
@@ -49,24 +20,9 @@ function hintToDisplay(text)
 	DisplayHelpTextFromStringLabel(0, 0, 1, -1)
 end
 
--- BLIP FOR SPEEDCAMERA (START)
-
-local blips = {
-	-- 60KM/H ZONES
-	{title="Speedcamera (60KM/H)", colour=1, id=1, x = -524.2645, y = -1776.3569, z = 21.3384}, -- 60KM/H ZONE
-	
-	-- 80KM/H ZONES
-	{title="Speedcamera (80KM/H)", colour=1, id=1, x = 2506.0671, y = 4145.2431, z = 38.1054}, -- 80KM/H ZONE
-	
-	-- 120KM/H ZONES
-	{title="Speedcamera (120KM/H)", colour=1, id=1, x = 1584.9281, y = -993.4557, z = 59.3923}, -- 120KM/H ZONE
-	{title="Speedcamera (120KM/H)", colour=1, id=1, x = 2442.2006, y = -134.6004, z = 88.7765}, -- 120KM/H ZONE
-	{title="Speedcamera (120KM/H)", colour=1, id=1, x = 2871.7951, y = 3540.5795, z = 53.0930} -- 120KM/H ZONE
-}
-
 Citizen.CreateThread(function()
-	for _, info in pairs(blips) do
-		if useBlips == true then
+	for _, info in pairs(Config.blips) do
+		if Config.useBlips == true then
 			info.blip = AddBlipForCoord(info.x, info.y, info.z)
 			SetBlipSprite(info.blip, info.id)
 			SetBlipDisplay(info.blip, 4)
@@ -80,21 +36,7 @@ Citizen.CreateThread(function()
 	end
 end)
 
--- BLIP FOR SPEEDCAMERA (END)
 
-local Speedcamera60Zone = {
-    {x = -524.2645,y = -1776.3569,z = 21.3384}
-}
-
-local Speedcamera80Zone = {
-    {x = 2506.0671,y = 4145.2431,z = 38.1054}
-}
-
-local Speedcamera120Zone = {
-    {x = 1584.9281,y = -993.4557,z = 59.3923},
-    {x = 2442.2006,y = -134.6004,z = 88.7765},
-    {x = 2871.7951,y = 3540.5795,z = 53.0930}
-}
 
 -- 60 ZONE (START)
 
@@ -102,10 +44,10 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
 
-        for k in pairs(Speedcamera60Zone) do
+        for k in pairs(Config.Speedcamera60Zone) do
 
             local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
-            local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, Speedcamera60Zone[k].x, Speedcamera60Zone[k].y, Speedcamera60Zone[k].z)
+            local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, Config.Speedcamera60Zone[k].x, Config.Speedcamera60Zone[k].y, Config.Speedcamera60Zone[k].z)
 
             if dist <= 20.0 then
 				local playerPed = GetPlayerPed(-1)
@@ -118,35 +60,27 @@ Citizen.CreateThread(function()
 					if IsPedInAnyVehicle(playerPed, false) then
 						if (GetPedInVehicleSeat(playerCar, -1) == playerPed) then
 							if hasBeenCaught == false then
-								if GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICE" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICE2" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICE3" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICE4" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICEB" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICET" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "FIRETRUK" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "AMBULAN" then -- BLACKLISTED VEHICLE
-								-- VEHICLES ABOVE ARE BLACKLISTED
+							if IsInAuthorizedVehicle() then
 								else
 									-- ALERT POLICE (START)
-									if alertPolice == true then
-										if SpeedKM > alertSpeed then
+									if Config.alertPolice == true then
+										if SpeedKM > Config.alertSpeed then
 											local x,y,z = table.unpack(GetEntityCoords(GetPlayerPed(-1), false))
-											TriggerServerEvent('esx_phone:send', 'police', ' Someone passed the speed camera, above ' .. alertSpeed.. ' KMH', true, {x =x, y =y, z =z})
+											TriggerServerEvent('esx_phone:send', 'police', ' Someone passed the speed camera, above ' .. Config.alertSpeed.. ' KMH', true, {x =x, y =y, z =z})
 										end
 									end
 									-- ALERT POLICE (END)								
 								
 									-- FLASHING EFFECT (START)
-									if useFlashingScreen == true then
+									if Config.useFlashingScreen == true then
 										TriggerServerEvent('esx_speedcamera:openGUI')
 									end
 									
-									if useCameraSound == true then
+									if Config.useCameraSound == true then
 										TriggerServerEvent("InteractSound_SV:PlayOnSource", "speedcamera", 0.5)
 									end
 									
-									if useFlashingScreen == true then
+									if Config.useFlashingScreen == true then
 										Citizen.Wait(200)
 										TriggerServerEvent('esx_speedcamera:closeGUI')
 									end
@@ -154,7 +88,7 @@ Citizen.CreateThread(function()
 								
 									TriggerEvent("pNotify:SendNotification", {text = "You've been caught by the speedcamera in a 60 zone!", type = "error", timeout = 5000, layout = "centerLeft"})
 									
-									if useBilling == true then
+									if Config.useBilling == true then
 										TriggerServerEvent('esx_billing:sendBill', GetPlayerServerId(PlayerId()), 'society_police', 'Speedcamera (60KM/H)', 500) -- Sends a bill from the police
 									else
 										TriggerServerEvent('esx_speedcamera:PayBill60Zone')
@@ -182,10 +116,10 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
 
-        for k in pairs(Speedcamera80Zone) do
+        for k in pairs(Config.Speedcamera80Zone) do
 
             local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
-            local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, Speedcamera80Zone[k].x, Speedcamera80Zone[k].y, Speedcamera80Zone[k].z)
+            local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, Config.Speedcamera80Zone[k].x, Config.Speedcamera80Zone[k].y, Config.Speedcamera80Zone[k].z)
 
             if dist <= 20.0 then
 				local playerPed = GetPlayerPed(-1)
@@ -198,35 +132,27 @@ Citizen.CreateThread(function()
 					if IsPedInAnyVehicle(playerPed, false) then
 						if (GetPedInVehicleSeat(playerCar, -1) == playerPed) then					
 							if hasBeenCaught == false then
-								if GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICE" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICE2" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICE3" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICE4" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICEB" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICET" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "FIRETRUK" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "AMBULAN" then -- BLACKLISTED VEHICLE
-								-- VEHICLES ABOVE ARE BLACKLISTED
+								if IsInAuthorizedVehicle() then
 								else
 									-- ALERT POLICE (START)
-									if alertPolice == true then
-										if SpeedKM > alertSpeed then
+									if Config.alertPolice == true then
+										if SpeedKM > Config.alertSpeed then
 											local x,y,z = table.unpack(GetEntityCoords(GetPlayerPed(-1), false))
-											TriggerServerEvent('esx_phone:send', 'police', ' Someone passed the speed camera, above ' .. alertSpeed.. ' KMH', true, {x =x, y =y, z =z})
+											TriggerServerEvent('esx_phone:send', 'police', ' Someone passed the speed camera, above ' .. Config.alertSpeed.. ' KMH', true, {x =x, y =y, z =z})
 										end
 									end
 									-- ALERT POLICE (END)								
 								
 									-- FLASHING EFFECT (START)
-									if useFlashingScreen == true then
+									if Config.useFlashingScreen == true then
 										TriggerServerEvent('esx_speedcamera:openGUI')
 									end
 									
-									if useCameraSound == true then
+									if Config.useCameraSound == true then
 										TriggerServerEvent("InteractSound_SV:PlayOnSource", "speedcamera", 0.5)
 									end
 									
-									if useFlashingScreen == true then
+									if Config.useFlashingScreen == true then
 										Citizen.Wait(200)
 										TriggerServerEvent('esx_speedcamera:closeGUI')
 									end
@@ -234,7 +160,7 @@ Citizen.CreateThread(function()
 								
 									TriggerEvent("pNotify:SendNotification", {text = "You've been caught by the speedcamera in a 80 zone!", type = "error", timeout = 5000, layout = "centerLeft"})
 									
-									if useBilling == true then
+									if Config.useBilling == true then
 										TriggerServerEvent('esx_billing:sendBill', GetPlayerServerId(PlayerId()), 'society_police', 'Speedcamera (80KM/H)', 1000) -- Sends a bill from the police
 									else
 										TriggerServerEvent('esx_speedcamera:PayBill80Zone')
@@ -262,10 +188,10 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
 
-        for k in pairs(Speedcamera120Zone) do
+        for k in pairs(Config.Speedcamera120Zone) do
 
             local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
-            local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, Speedcamera120Zone[k].x, Speedcamera120Zone[k].y, Speedcamera120Zone[k].z)
+            local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, Config.Speedcamera120Zone[k].x, Config.Speedcamera120Zone[k].y, Config.Speedcamera120Zone[k].z)
 
             if dist <= 20.0 then
 				local playerPed = GetPlayerPed(-1)
@@ -278,35 +204,27 @@ Citizen.CreateThread(function()
 					if IsPedInAnyVehicle(playerPed, false) then
 						if (GetPedInVehicleSeat(playerCar, -1) == playerPed) then 
 							if hasBeenCaught == false then
-								if GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICE" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICE2" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICE3" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICE4" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICEB" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "POLICET" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "FIRETRUK" then -- BLACKLISTED VEHICLE
-								elseif GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "AMBULAN" then -- BLACKLISTED VEHICLE
-								-- VEHICLES ABOVE ARE BLACKLISTED
+								if IsInAuthorizedVehicle() then
 								else
 									-- ALERT POLICE (START)
-									if alertPolice == true then
-										if SpeedKM > alertSpeed then
+									if Config.alertPolice == true then
+										if SpeedKM > Config.alertSpeed then
 											local x,y,z = table.unpack(GetEntityCoords(GetPlayerPed(-1), false))
-											TriggerServerEvent('esx_phone:send', 'police', ' Someone passed the speed camera, above ' .. alertSpeed.. ' KMH', true, {x =x, y =y, z =z})
+											TriggerServerEvent('esx_phone:send', 'police', ' Someone passed the speed camera, above ' .. Config.alertSpeed.. ' KMH', true, {x =x, y =y, z =z})
 										end
 									end
 									-- ALERT POLICE (END)
 								
 									-- FLASHING EFFECT (START)
-									if useFlashingScreen == true then
+									if Config.useFlashingScreen == true then
 										TriggerServerEvent('esx_speedcamera:openGUI')
 									end
 									
-									if useCameraSound == true then
+									if Config.useCameraSound == true then
 										TriggerServerEvent("InteractSound_SV:PlayOnSource", "speedcamera", 0.5)
 									end
 									
-									if useFlashingScreen == true then
+									if Config.useFlashingScreen == true then
 										Citizen.Wait(200)
 										TriggerServerEvent('esx_speedcamera:closeGUI')
 									end
@@ -314,7 +232,7 @@ Citizen.CreateThread(function()
 								
 									TriggerEvent("pNotify:SendNotification", {text = "You've been caught by the speedcamera in a 120 zone!", type = "error", timeout = 5000, layout = "centerLeft"})
 									
-									if useBilling == true then
+									if Config.useBilling == true then
 										TriggerServerEvent('esx_billing:sendBill', GetPlayerServerId(PlayerId()), 'society_police', 'Speedcamera (120KM/H)', 1500) -- Sends a bill from the police
 									else
 										TriggerServerEvent('esx_speedcamera:PayBill120Zone')
@@ -335,6 +253,19 @@ Citizen.CreateThread(function()
 end)
 
 -- 120 ZONE (END)
+
+function IsInAuthorizedVehicle()
+	local playerPed = PlayerPedId()
+	local vehModel  = GetEntityModel(GetVehiclePedIsIn(playerPed, false))
+
+	for i=1, #Config.AuthorizedVehicles, 1 do
+		if vehModel == GetHashKey(Config.AuthorizedVehicles[i].model) then
+			return true
+		end
+	end
+	
+	return false
+end
 
 RegisterNetEvent('esx_speedcamera:openGUI')
 AddEventHandler('esx_speedcamera:openGUI', function()
