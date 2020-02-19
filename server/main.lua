ec842356f8e42ce015f84daefe2269d3878875ff
P4NDAzz <36475554+P4NDAzzGaming@users.noreply.github.com>
2020-02-19 15:05:39 +0100

    Update main.lua
    New update!
    - Changed billing prices for those who don't use "esx_billing".
    - Cleaned up the code.

--- server/main.lua
+++ server/main.lua
@@ -5,32 +5,29 @@ TriggerEvent('esx:getSharedObject', function(obj)
 end)
 
 -- BILLS WITHOUT ESX_BILLING (START)
-
 RegisterServerEvent('esx_speedcamera:PayBill60Zone')
 AddEventHandler('esx_speedcamera:PayBill60Zone', function()
 	local xPlayer = ESX.GetPlayerFromId(source)
 	
-	xPlayer.removeMoney(500)
+	xPlayer.removeMoney(100)
 end)
 
 RegisterServerEvent('esx_speedcamera:PayBill80Zone')
 AddEventHandler('esx_speedcamera:PayBill80Zone', function()
 	local xPlayer = ESX.GetPlayerFromId(source)
 	
-	xPlayer.removeMoney(1000)	
+	xPlayer.removeMoney(300)	
 end)
 
 RegisterServerEvent('esx_speedcamera:PayBill120Zone')
 AddEventHandler('esx_speedcamera:PayBill120Zone', function()
 	local xPlayer = ESX.GetPlayerFromId(source)
 	
-	xPlayer.removeMoney(1500)
+	xPlayer.removeMoney(500)
 end)
-
 -- BILLS WITHOUT ESX_BILLING (END)
 
 -- FLASHING EFFECT (START)
-
 RegisterServerEvent('esx_speedcamera:openGUI')
 AddEventHandler('esx_speedcamera:openGUI', function()
 	TriggerClientEvent('esx_speedcamera:openGUI', source)
@@ -40,7 +37,6 @@ RegisterServerEvent('esx_speedcamera:closeGUI')
 AddEventHandler('esx_speedcamera:closeGUI', function()
 	TriggerClientEvent('esx_speedcamera:closeGUI', source)
 end)
-
 -- FLASHING EFFECT (END)
 
 function notification(text)
